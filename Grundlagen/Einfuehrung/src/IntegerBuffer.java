233104e31095cf6953d1c7c5366a0e2a61653ed3
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-04-02 10:33:00 +0200

    Made some correction in the interface IntegerBuffer.

--- Grundlagen/Einfuehrung/src/IntegerBuffer.java
+++ Grundlagen/Einfuehrung/src/IntegerBuffer.java
@@ -1,6 +1,6 @@
 public interface IntegerBuffer {
-    public void push(Integer i); //Speichert ein Objekt im Speicher.
-    public Integer pop();        //Liefert ein Objekt vom Speicher zurueck.
-    public int size();           //Gibt die Anzahl von Objekten zurueck.
-    public int capacity();       //Gibt die Groesse des Speichers zurueck.
+    public void push(Integer i)throws IllegalArgumentException,StackOverFlowExceptions; //Speichert ein Objekt im Speicher.
+    public Integer pop()throws ArrayIndexOutOfBoundsException;                          //Liefert ein Objekt vom Speicher zurueck.
+    public int size();                                                                 //Gibt die Anzahl von Objekten zurueck.
+    public int capacity();                                                             //Gibt die Groesse des Speichers zurueck.
 }
