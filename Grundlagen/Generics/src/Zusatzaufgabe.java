0040c95dbda481a31f87a38b534c54860853f362
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-04-04 15:37:12 +0200

    Corrected the mistake in the sum method for the Zusatzaufgabe.

--- Grundlagen/Generics/src/Zusatzaufgabe.java
+++ Grundlagen/Generics/src/Zusatzaufgabe.java
@@ -5,12 +5,12 @@ public class Zusatzaufgabe {
         int sum = 0;
 
         for(int i = 1;i < range;i++){
-            if((sum1 *=i) < range){
-                sum += sum1 * i;
+            if((sum1 * i) < range){
+                sum += (sum1 * i);
             }
 
-            if((sum2 *=i) < range){
-                sum += sum2 * i;
+            if((sum2 * i) < range){
+                sum += (sum2 * i);
             }
         }
         return sum;
