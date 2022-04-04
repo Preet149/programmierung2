38fd23004e12dd78813d6e1140b02935b026b9e1
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-04-04 15:38:11 +0200

    .....

--- Grundlagen/Generics/src/Main.java
+++ Grundlagen/Generics/src/Main.java
@@ -1,20 +1,4 @@
 public class Main {
     public static void main(String[]args) throws StackOverFlowExceptions {
-        Fifo<Integer>f = new Fifo<>(5);
-        System.out.println(f.capacity());
-
-        for(int i = 0;i < f.capacity();i++){
-            f.push(i);
-        }
-
-        for(int i = 0;i < f.capacity();i++){
-            System.out.println(f.pop());
-        }
-
-
-
-
-
-
     }
 }
