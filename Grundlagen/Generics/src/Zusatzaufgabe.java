893c12642cbd307dc3309b5ec12f238e5b648b50
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-04-04 15:24:39 +0200

    Made a class Zusatzaufgabe.

---
+++ Grundlagen/Generics/src/Zusatzaufgabe.java
@@ -0,0 +1,18 @@
+public class Zusatzaufgabe {
+    public static int sum(int range){
+        int sum1 = 3;
+        int sum2 = 5;
+        int sum = 0;
+
+        for(int i = 1;i < range;i++){
+            if((sum1 *=i) < range){
+                sum += sum1 * i;
+            }
+
+            if((sum2 *=i) < range){
+                sum += sum2 * i;
+            }
+        }
+        return sum;
+    }
+}
