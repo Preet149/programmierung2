5ef2f0fa05f9a1855949517b8da88faeb3e3f2bd
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-04-04 12:40:06 +0200

    Made a slight correction in the pop method for the Fifo class.

--- Grundlagen/Generics/src/Fifo.java
+++ Grundlagen/Generics/src/Fifo.java
@@ -14,13 +14,13 @@ public class Fifo<T> extends AbstractBuffer<T>{
 
     //Gibt das aller erstes Objekt zurueck.
     public T pop()throws ArrayIndexOutOfBoundsException {
-        if(size() < 1) {
+        if(size() == 0) {
             throw new ArrayIndexOutOfBoundsException();
         }
         T value = array[0];
 
         //Jedes Objekt um einen Index nach links verschieben.
-        for(int i = 0;i < size;i++) {
+        for(int i = 0;i < size-1;i++) {
             array[i] = array[i+1];
         }
         size--;
