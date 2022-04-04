c6e0182fcd157aaaf5483c366d625015caf19fdf
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-04-04 15:37:52 +0200

    Made a ZusatzaufgabeTest to test the method sum in the class Zusatzaufgabe.

---
+++ Grundlagen/Generics/src/ZusatzaufgabeTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+ *author: Preet
+ */class ZusatzaufgabeTest {
+
+    @Test
+    void testsum() {
+        assertEquals(23,Zusatzaufgabe.sum(10),"The sum is not correct!!");
+    }
+}
