3834e269b14415610b1715e2ac344724f5feccda
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-04-11 11:54:14 +0200

    Made a EVLTest class.

---
+++ Grundlagen/Verkettete Liste/src/EVLTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+ *author: Preet
+ */class EVLTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFirst() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLast() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addLast() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+}
